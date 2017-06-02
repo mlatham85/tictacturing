32cc61970b2799ef96dc1c11d28ff5b3b951090c
Matt Latham <lath.mj@gmail.com>
2017-06-02 12:05:52 -0700

    Finish initial routing

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,14 +1,18 @@
 import React from 'react'
 import { Route, IndexRoute } from 'react-router'
+
+// Components
 import Template from '../containers/Template'
+import Home from '../containers/Home'
+import Profile from '../containers/Profile'
 
 const createRoutes = () => {
     return (
-        <Route
-            path='/'
-            component={Template}
-        >
-
+        <Route path='/'
+            component={Template} >
+            <IndexRoute component={Home} />
+            <Route path={'/profile'}
+                component={Profile} />
         </Route>
     )
 }
