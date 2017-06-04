3e6c833151f18bcad38eaaeebea037f2fceb7c88
Matt Latham <lath.mj@gmail.com>
2017-06-04 00:41:10 -0700

    auth0 service

---
+++ src/utils/auth.js
@@ -0,0 +1,71 @@
+import Auth0Lock from 'auth0-lock'
+
+const authDomain = 'mlatham.auth0.com'
+const clientId   = 'LI9nuQAO95T1443RSada47xuHiT6a3PL'
+
+class AuthService {
+    constructor() {
+        this.lock = new Auth0Lock(clientId, authDomain, {
+            auth: {
+                params: {
+                    scope: 'openid email'
+                },
+            },
+        })
+
+        this.showLock = this.showLock.bind(this)
+
+        this.lock.on('authenticated', this.authProcess.bind(this))
+    }
+
+    authProcess = (authResult) => {
+        console.log(authResult)
+    }
+
+    showLock() {
+        this.lock.show()
+    }
+
+    setToken = (authFields) => {
+        let {
+            idToken,
+            exp
+        } = authFields
+        localStorage.setItem('idToken', idToken)
+        localStorage.setItem('exp', exp * 1000)
+    }
+
+    isCurrent = () => {
+        let expString = localStorage.getItem('exp')
+        if (!expString) {
+            localStorage.removeItem('idToken')
+            return false
+        }
+        let now = new Date()
+        let exp = new Date(parseInt(expString, 10)) // 10 is radix parameter
+        if (exp < now) {
+            this.logout()
+            return false
+        } else {
+            return true
+        }
+    }
+
+    getToken() {
+        let idToken = localStorage.getItem('idToken')
+        if (this.isCurrent() && idToken) {
+            return idToken
+        } else {
+            localStorage.removeItem('idToken')
+            localStorage.removeItem('exp')
+            return false
+        }
+    }
+
+    logout = () => {
+        localStorage.removeItem('idToken')
+        localStorage.removeItem('exp')
+        location.reload()
+    }
+
+}
